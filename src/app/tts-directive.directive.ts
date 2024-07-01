0facc034b6b3f02dc1404dc90144cc677d8bf69a
Jade <jade.hanonge@mind-and-go.com>
2024-07-01 08:48:59 +0200

    nettoyage du code

--- src/app/tts-directive.directive.ts
+++ src/app/tts-directive.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener} from '@angular/core';
 export class TtsDirectiveDirective {
 
   constructor(private el: ElementRef) { }
-  private text = '';
+  private text!: string;
 
   extractText(element: HTMLElement, clikedElementClasse: string) {
 
@@ -20,29 +20,7 @@ export class TtsDirectiveDirective {
       }
     }
     
-
-   //for (let child of children) {
-    //if (child)
-      // if (child.nodeType === Node.TEXT_NODE){
-      //   this.text += child.nodeValue;
-      //   console.log("text node: " + this.text);
-        
-        
-      // }else if (child.nodeType === Node.ELEMENT_NODE){
-      //   const el = child as HTMLElement;
-      //  if (el.className != clikedElementClasse) {
-      //     this.text += this.extractText(el, clikedElementClasse)
-      //     console.log("text element node : " + this.text);
-          
-      //   }
-      // }
-  //}
-    console.log("children : " + children);
-    console.log("text methode : " + this.text);
-    
-    return this.text;
-    
-    
+    return this.text; 
 
   }
 
@@ -50,15 +28,13 @@ export class TtsDirectiveDirective {
   @HostListener('click', ['$event']) onClick(event: Event) {
     
     const clikedElement = event.target as HTMLElement
-    let test = this.el.nativeElement.innerText;
-    console.log("test : " + test);
     
     if (clikedElement && clikedElement.className === 'btnListen'){
       event.stopPropagation();
       this.text = this.extractText(this.el.nativeElement, clikedElement.className)
     }
-    console.log("text : " + this.text);
-    console.log("native element : " + this.el.nativeElement);
+    // console.log("text : " + this.text);
+    // console.log("native element : " + this.el.nativeElement);
     
     
     //envoie le text au serveur
@@ -80,38 +56,6 @@ export class TtsDirectiveDirective {
     .catch(error => {
       console.error('Erreur:', error);
     });
-
-    //fonction pour recuperer le text contenue dans la div.
-    // function extractText(element: HTMLElement, clikedElementID: string) {
-
-    //   let text = ' ';
-    //   let children = Array.from(element.childNodes);
-
-    //  for (let child of children) {
-    //     if (child.nodeType === Node.TEXT_NODE){
-    //       text += child.nodeValue;
-    //       console.log("text node: " + text);
-          
-          
-    //     }else if (child.nodeType === Node.ELEMENT_NODE){
-    //       const el = child as HTMLElement;
-    //      if (el.id != clikedElementID) {
-    //         text += extractText(el, clikedElementID)
-    //         console.log("text element node : " + text);
-            
-    //       }
-    //     }
-    // }
-    //   console.log("children : " + children);
-    //   return text;
-      
-      
-
-    // }
-
-    
-
-    
     
   }
 
